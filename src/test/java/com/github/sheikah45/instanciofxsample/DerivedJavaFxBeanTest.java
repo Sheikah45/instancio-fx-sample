828b6b8073e96f75118b9d2e7548633ff7a2b632
Sheikah45 <Sheikah450@gmail.com>
2024-01-08 16:52:24 -0500

    Update to Instancio 4.0.0

--- src/test/java/com/github/sheikah45/instanciofxsample/DerivedJavaFxBeanTest.java
+++ src/test/java/com/github/sheikah45/instanciofxsample/DerivedJavaFxBeanTest.java
@@ -1,12 +1,14 @@
 package com.github.sheikah45.instanciofxsample;
 
 import org.instancio.Instancio;
-import org.instancio.assignment.AssignmentType;
-import org.instancio.assignment.OnSetMethodNotFound;
-import org.instancio.assignment.SetterStyle;
 import org.instancio.junit.InstancioExtension;
 import org.instancio.junit.WithSettings;
+import org.instancio.settings.AssignmentType;
 import org.instancio.settings.Keys;
+import org.instancio.settings.OnSetFieldError;
+import org.instancio.settings.OnSetMethodNotFound;
+import org.instancio.settings.OnSetMethodUnmatched;
+import org.instancio.settings.SetterStyle;
 import org.instancio.settings.Settings;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -19,7 +21,9 @@ class DerivedJavaFxBeanTest {
     @WithSettings
     private static final Settings SETTINGS = Settings.create()
                                                      .set(Keys.ASSIGNMENT_TYPE, AssignmentType.METHOD)
+                                                     .set(Keys.ON_SET_FIELD_ERROR, OnSetFieldError.IGNORE)
                                                      .set(Keys.SETTER_STYLE, SetterStyle.SET)
+                                                     .set(Keys.ON_SET_METHOD_UNMATCHED, OnSetMethodUnmatched.INVOKE)
                                                      .set(Keys.ON_SET_METHOD_NOT_FOUND, OnSetMethodNotFound.IGNORE);
 
     @Test
@@ -27,7 +31,7 @@ class DerivedJavaFxBeanTest {
         var derivedBean = Instancio.create(DerivedJavaFxBean.class);
 
         assertNotNull(derivedBean.getSource());
-        assertTrue(derivedBean.getDerived() > 0);
+        assertEquals(derivedBean.getSource().length(), derivedBean.getDerived());
     }
 
 }
